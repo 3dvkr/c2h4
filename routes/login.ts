5fca7ad63d04731741638c317502b85c9eaab626
dvkr <v3dvkr@gmail.com>
2022-10-06 19:20:22 -0600

    added input validation to login routes

--- routes/login.ts
+++ routes/login.ts
@@ -1,51 +1,84 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from "express";
+import { z, AnyZodObject } from "zod";
 
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 import session from "express-session";
-interface ISessionData
-    extends session.Session {
-    username: string;
+interface ISessionData extends session.Session {
+  username: string;
 }
+
+const signUpSchema = z.object({
+  username: z
+    .string({
+      required_error: "User name is required",
+    })
+    .min(4)
+    .max(10),
+  email: z
+    .string({
+      required_error: "Email is required",
+    })
+    .email("Not a valid email"),
+  password: z.string({
+    required_error: "Password is required",
+  }),
+  confirmPassword: z.string({
+    required_error: "Password confirmation is required",
+  }),
+});
+const validateSignUp = (schema: AnyZodObject) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema
+        .refine((data) => data.password === data.confirmPassword, {
+          message: "Passwords do not match",
+          path: ["password"],
+        })
+        .parseAsync(req.body);
+      return next();
+    } catch (error) {
+      return res.status(400).json({ error });
+    }
+  };
+};
 
 export const authRouter = Router();
 
-authRouter.post(`/sign-up`,
-    async (
-        req: Request,
-        res: Response
-    ) => {
-        if (req.body.username?.length > 0) {
-            const {username, password, email} = req.body
-            console.log(`found body.name`);
-            console.log(req?.session);
-            const user = await prisma.user.create({
-                data: req.body
-            });
-            (<ISessionData>req.session).username = req.body.username;
-            res.json({message: 'ok'})
-        } else {
-            res.send({
-                title: `Express Login`,
-                errorMessage: `Please enter a username and password`
-            })
-        }
-    });
+authRouter.post(
+  `/sign-up`,
+  validateSignUp(signUpSchema),
+  async (req: Request, res: Response) => {
+    try {
+      const { username, password, email } = req.body;
+      const user = await prisma.user.create({
+        data: {
+          username,
+          password,
+          email,
+        },
+      });
+      (<ISessionData>req.session).username = req.body.username;
+      res.json({ message: "ok" });
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  }
+);
 
-    authRouter.post('/login', async (req: Request, res: Response) => {
-        const user = await prisma.user.findFirst({
-            where: {
-                username: req.body.username
-            },
-            include: {
-                item: {
-                    orderBy: {
-                        expiry: 'asc' // earlier dates first, far future dates last
-                    }
-                }
-            }
-        });
-        console.log(user);
-        res.send()
-    })
+authRouter.post("/login", async (req: Request, res: Response) => {
+  const user = await prisma.user.findFirst({
+    where: {
+      username: req.body.username,
+    },
+    include: {
+      item: {
+        orderBy: {
+          expiry: "asc", // earlier dates first, far future dates last
+        },
+      },
+    },
+  });
+  res.send();
+});
